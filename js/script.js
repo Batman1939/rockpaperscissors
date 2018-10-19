474ef5e1e66345d880992a288d8e55acf4d56cc9
Luis A Bonilla <luis.bonilla@amethodschools.org>
2018-10-19 01:19:38 +0000

    Created computer choice options

--- js/script.js
+++ js/script.js
@@ -8,6 +8,21 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = 0;
 $("shoot").click(function(){
-    $("#input").text($("#userChoice").val())
+    var userChoice = ("#input").val();
+    $("#input").text($("#userChoice").val());
+    var randomNumber = Math.random(); 
+    if (randomNumber >= 2/3)  {
+        $("computerChoice").text("Rock");
+    }
+    else if (randomNumber <= 2/3 && randomNumber >= 1/3) {
+        $("comuterChoice").text("Paper");
+    }
+    else if (randomNumber <= 1/3 && randomNumber >= 0) {
+        $("computerChoice").text("scissors");
+    }
 });
